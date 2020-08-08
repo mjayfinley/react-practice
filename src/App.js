d5d78dfaa679e15dbfee430bdf4d418fa7a2be04
Micah Finley <mjayfinley@gmail.com>
2020-08-08 13:02:20 -0500

    Use state example

--- src/App.js
+++ src/App.js
@@ -2,9 +2,56 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [developer, setDeveloper] = React.useState({
+        language: "",
+        yearsExperience: 0,
+        isEmployed: false,
+        employmentStatus: ""
+    })
+
+    function handleChangeYearsExperience(event) {
+        const years = event.target.value
+
+        setDeveloper({ ...developer, yearsExperience: years })
+    }
+
+    function handleToggleEmployment(event) {
+
+        setDeveloper(prevState => {
+            return { ...prevState, isEmployed: !prevState.isEmployed }
+        })
+
+        if (developer.isEmployed) {
+            developer.employmentStatus = "Employed"
+        } else {
+            developer.employmentStatus = "Un-employed"
+        }
+    }
+
     return (
         <div className="App">
-            <p>React App</p>
+            <button
+                onClick={() =>
+                    setDeveloper({
+                        language: "javascript",
+                        yearsExperience: 0
+                    })
+                }
+            >
+                Change language to JS
+            </button>
+
+            <input
+                type="number"
+                value={developer.yearsExperience}
+                onChange={handleChangeYearsExperience}
+            />
+
+            <button onClick={handleToggleEmployment}>Toggle Employement Status</button>
+
+            <p>I am now learning {developer.language}.</p>
+            <p>I have {developer.yearsExperience} years of experience.</p>
+            <p>I am {developer.employmentStatus}</p>
         </div>
     );
 }
