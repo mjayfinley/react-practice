e15b91a5852c642a0fe1345dddb2e1dc50422618
Micah Finley <mjayfinley@gmail.com>
2020-08-08 19:55:41 -0500

    useEffect example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
 
+const endpoint = "https://jsonplaceholder.typicode.com/todos/1";
+
 function App() {
+    const [user, setUser] = React.useState(null);
+
+    React.useEffect(() => {
+        getUser();
+    }, []);
+
+    async function getUser() {
+        const response = await fetch(endpoint);
+
+        const data = await response.json();
+        console.log(data)
+        setUser(data.title)
+    }
+
     return (
         <div className="App">
-            <p>React App</p>
+            <p>{user}</p>
         </div>
     );
 }
