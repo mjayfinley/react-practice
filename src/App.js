27cd36b5dbe5fb076ba425b601492058422fc193
Micah Finley <mjayfinley@gmail.com>
2020-08-08 20:01:57 -0500

    useCallback example

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [time, setTime] = React.useState();
+    const [count, setCount] = React.useState(0);
+
+    const inc = React.useCallback(
+        function handleIncrementCount() {
+            setCount(prevCount => prevCount + 1);
+        },
+
+        [setCount]
+    );
+
+    React.useEffect(() => {
+        const timeout = setTimeout(() => {
+            const currentTime = JSON.stringify(new Date(Date.now()));
+            setTime(currentTime);
+        }, 300);
+
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [time]);
+
     return (
         <div className="App">
-            <p>React App</p>
+            <p>The current time is: {time}</p>
+            <p>Count: {count}</p>
+            <button onClick={inc}>+</button>
         </div>
     );
 }
