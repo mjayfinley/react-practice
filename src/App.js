530bdb2e7ee8008f5df0bfc6ddc03643c409f3ba
Micah Finley <mjayfinley@gmail.com>
2020-08-08 20:15:44 -0500

    useRef example

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [query, setQuery] = React.useState("");
+
+    const searchInput = React.useRef(null);
+
+    function handleClearSearch() {
+        searchInput.current.value = "";
+        searchInput.current.focus();
+    }
+
     return (
         <div className="App">
-            <p>React App</p>
+            <form>
+                <input
+                    type="text"
+                    onChange={event => setQuery(event.target.value)}
+                    ref={searchInput}
+                />
+                <button type="submit">Search</button>
+                <button type="button" onClick={handleClearSearch}>Clear</button>
+            </form>
+
+            <p>{query}</p>
         </div>
     );
 }
