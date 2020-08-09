7a7580f091daaadde738dbf6b7006f3fe70c54f2
Micah Finley <mjayfinley@gmail.com>
2020-08-08 20:10:24 -0500

    useMemo example

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+    const [wordIndex, setWordIndex] = React.useState(0);
+    const [count, setCount] = React.useState(0);
+
+    const words = ["i", "am", "learning", "react"];
+    const word = words[wordIndex]
+
+    function getLetterCount(word) {
+        let i = 0;
+        while (i < 1000000) i++;
+        return word.length
+    }
+
+    const letterCount = React.useMemo(() => getLetterCount(word), [word]);
+
+    function handleChangeIndex() {
+        const next = wordIndex + 1 === words.length ? 0 : wordIndex + 1;
+        setWordIndex(next);
+    }
+
     return (
         <div className="App">
-            <p>React App</p>
+            <p>
+                {word} has {letterCount} letters.
+            </p>
+            <button onClick={handleChangeIndex}>Next Word</button>
+            <p>Counter: {count}</p>
+            <button onClick={() => setCount(count + 1)}>+</button>
         </div>
     );
 }
