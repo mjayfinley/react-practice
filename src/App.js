b6845402aebbd09709abdd71b6cb90dfd432dd48
Micah Finley <mjayfinley@gmail.com>
2020-08-09 19:17:41 -0500

    customHooks example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 
+function useAPI(endpoint) {
+    const [value, setValue] = React.useState([]);
+
+    React.useEffect(() => {
+        getData();
+    }, []);
+
+    async function getData() {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        setValue(data);
+    }
+
+    return value;
+}
+
 function App() {
+    const todos = useAPI("https://jsonplaceholder.typicode.com/todos")
+
     return (
-        <div className="App">
-            <p>React App</p>
-        </div>
+        <ul>
+            {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+        </ul>
     );
 }
 
