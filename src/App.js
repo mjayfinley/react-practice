9d7f187431eaca1d562cc2f8f05f2c230c12f90b
Micah Finley <mjayfinley@gmail.com>
2020-08-09 19:01:59 -0500

    useReducer example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
+const initialState = { username: "", isAuth: false };
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "LOGIN":
+            return { username: action.payload.username, isAuth: true };
+        case "SIGNOUT":
+            return { username: "", isAuth: false };
+        default:
+            return state;
+    }
+}
+
 function App() {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    function handleLogin() {
+        dispatch({ type: "LOGIN", payload: { username: "Ted" } });
+    }
+
+    function handleSignout() {
+        dispatch({ type: "SIGNOUT" });
+    }
+
     return (
-        <div className="App">
-            <p>React App</p>
-        </div>
+        <>
+            Current user: {state.username}, isAuthenticated: {state.isAuth}
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleSignout}>Signout</button>
+        </>
     );
 }
 
